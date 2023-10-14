bedcd0d211df40eb0fd190163b50a33196a6f07c
Musa Kocabas <mkocabas@k2m03s02.42kocaeli.com.tr>
2023-10-14 18:22:25 +0300

    player position update v1

--- src/player_position.c
+++ src/player_position.c
@@ -12,37 +12,54 @@
 
 #include "../includes/cub3d.h"
 
+static void setplane(t_game *game, char c)
+{
+	if (c == 'N')
+	{
+		game->ray.planex = 1;
+		game->ray.planey = 0;
+	}
+	else if (c == 'S')
+	{
+		game->ray.planex = -1;
+		game->ray.planey = 0;
+	}
+	else if (c == 'W')
+	{
+		game->ray.planex = 0;
+		game->ray.planey = -1;
+	}
+	else if (c == 'E')
+	{
+		game->ray.planex = 0;
+		game->ray.planey = 1;
+	}
+}
+
 void	player_position(t_game *game, int i, int j)
 {
 	game->ray.posx = i + 0.5;
 	game->ray.posy = j + 0.5;
 
-	if (game->map[i][j] == 'W')
+	if (game->map[i][j] == 'N')
+	{
+		game->ray.dirx = 0;
+		game->ray.diry = -1;
+	}
+	else if (game->map[i][j] == 'S')
+	{
+		game->ray.dirx = 0;
+		game->ray.diry = 1;
+	}
+	else if (game->map[i][j] == 'W')
 	{
 		game->ray.dirx = -1;
 		game->ray.diry = 0;
-		game->ray.planex = 0;
-		game->ray.planey = -1;
 	}
 	else if (game->map[i][j] == 'E')
 	{
 		game->ray.dirx = 1;
 		game->ray.diry = 0;
-		game->ray.planex = 0;
-		game->ray.planey = 1;
 	}
-	else if (game->map[i][j] == 'N')
-	{
-		game->ray.dirx = 0;
-		game->ray.diry = 1;
-		game->ray.planex = 1;
-		game->ray.planey = 0;
-	}
-	else if (game->map[i][j] == 'S')
-	{
-		game->ray.dirx = 0;
-		game->ray.diry = -1;
-		game->ray.planex = -1;
-		game->ray.planey = 0;
-	}
+	setplane(game, game->map[i][j]);
 }
